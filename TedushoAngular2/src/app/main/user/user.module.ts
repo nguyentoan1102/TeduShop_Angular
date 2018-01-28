58e5f2a38fd01be93150af67ed490b0f7d30d5fb
nguyentoan1102 <longtinh12@gmail.com>
2018-01-28 21:47:26 +0700

    Initializer authentication

--- TedushoAngular2/src/app/main/user/user.module.ts
+++ TedushoAngular2/src/app/main/user/user.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { Routes, RouterModule } from '@angular/router';
-
 const userRoutes: Routes = [
-   //localhost:4200/main/user
+  // localhost:4200/main/user
   { path: '', redirectTo: 'index', pathMatch: 'full' },
-   //localhost:4200/main/user/index
+  // localhost:4200/main/user/index
   { path: 'index', component: UserComponent }
 ]
 @NgModule({
