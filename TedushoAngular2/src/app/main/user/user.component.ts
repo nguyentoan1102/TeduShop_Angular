6f02630140ace2bdc11a9e509d998aa67d46b40a
nguyentoan1102 <longtinh12@gmail.com>
2018-02-04 09:50:39 +0700

    Compleate Role

--- TedushoAngular2/src/app/main/user/user.component.ts
+++ TedushoAngular2/src/app/main/user/user.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ViewChild } from '@angular/core/src/metadata/di';
+import { ModalDirective } from 'ngx-bootstrap/modal/modal.directive';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  @ViewChild('modalAddEdit') public modalAddEdit: ModalDirective;
+  public pageIndex: number = 1;
+  public pageSize: number = 10;
+  public pageDisplay: number = 10;
+  public totalRow: number =
+    constructor() { }
+ngOnInit() {
+}
 }
--- TedushoAngular2/src/app/main/user/user.module.ts
+++ TedushoAngular2/src/app/main/user/user.module.ts
@@ -1,7 +1,12 @@
+import { NotificationService } from 'app/Core/services/notification.service';
+import { DataService } from './../../Core/services/data.service';
+import { FormsModule } from '@angular/forms';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { Routes, RouterModule } from '@angular/router';
+import { ModalModule } from 'ngx-bootstrap/modal';
 const userRoutes: Routes = [
   // localhost:4200/main/user
   { path: '', redirectTo: 'index', pathMatch: 'full' },
@@ -11,8 +16,12 @@ const userRoutes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    PaginationModule,
+    ModalModule.forRoot(),
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ],
-  declarations: [UserComponent]
+  declarations: [UserComponent],
+  providers: [DataService, NotificationService]
 })
 export class UserModule { }
